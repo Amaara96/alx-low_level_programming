a2b4e080b9af27c3fcbda97afcf07f28cc9ce7ff
Amaara <amara.alex96@gmail.com>
2023-10-18 02:29:30 +0000

    Kmart

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the alphabet in lowercase.
+  *
+  * Return: Always 0.
+ */
+int main(void)
+{
+	char l = 'a';
+
+	while (l <= 'z')
+		putchar(l);
+
+		l++;
+
+		putchar('\n');
+
+		return (0);
+}
