48445716879eebf5fb8b60da56d52b081fc96221
Amaara <amara.alex96@gmail.com>
2023-10-24 04:33:51 +0000

    Single digits only

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
-
 /**
- *  * main - Prints all possible combinations of single-digit numbers.
- *   *
- *    * Return: Always 0.
- *     */
+  * main - A program that prints all single digit numbers
+  *
+  * The numbers are seperated by a comma symbol
+  *
+  * Return: Always return to 0
+*/
 int main(void)
 {
-		int num;
+	int num;
 
-			for (num = 0; num <= 9; num++)
-					{
-								putchar((num % 10) + '0');
-										if (num == 9)
-														continue;
-												putchar(',');
-														putchar(' ');
-															}
+	for(num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		{
+			if(num == 9)
+			{
+				continue;
+			}	
+			putchar(',');
+					putchar(' ');
 
-				putchar('\n');
+			}
+		
+	}
+	
+	putchar('\n');
 
-					return (0);
+	return (0);
 }
