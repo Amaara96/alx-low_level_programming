840fb2523f3fa29cdce94379903cb86967b60592
Amaara <amara.alex96@gmail.com>
2023-11-05 05:17:29 +0000

    Kelp

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,17 +9,19 @@
 void print_alphabet_x10(void)
 {
 	int b = 0;
+	
 	while (b <= 10)
 		{
 			char c = 'a';
+	
 			while (c <= 'z')
 				{
 					_putchar(c);
 						c++;
 				}
-		putchar('\n');
-		b++;
+		_putchar('\n');
+			b++;
 
-		}
+		}	
 
 }
