6bddd2a4ae538e38527bb807883b402f548893b1
Amaara <amara.alex96@gmail.com>
2023-11-08 20:03:39 +0000

    Alphabets

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * _isalpha -  checks the code for ALX students
+  *
+  * Description: The code checks for alphabets only
+  *
+  * @c: the letter must either be uppercase or lowercase
+  *
+  * Return:  Always return to 0
+*/
+
+int _isalpha(int c)
+{
+	if (c == 67 || c == 99)
+		return (1);
+	else
+		return (0);
+}
