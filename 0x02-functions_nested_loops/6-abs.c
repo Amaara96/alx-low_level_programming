6fe02c4f53260fee7f9ec7e6d8e25c30b7234b3c
Amaara <amara.alex96@gmail.com>
2023-11-17 22:40:17 +0000

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * _abs - check main
+  *
+  * @j:  The absolute integer
+  *
+  * Description: The abs function computes the absolute value of the integer
+  *
+  * Return: Always return to 0
+*/
+
+int _abs(int j)
+
+{
+
+	if (j < 0)
+	{
+		return (j * -1);
+	}
+	return (j);
+
+}
