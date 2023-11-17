fc129685095188b68545701bd9bc36697724a2fb
Amaara <amara.alex96@gmail.com>
2023-11-17 02:41:27 +0000

    Singly

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
-
 /**
- *  * main - Prints all possible combinations of two different digits,
- *   *        in ascending order, separated by a comma followed by a space.
- *    *
- *     * Return: Always 0.
- *      */
+  * main - A program that prints two digit numbers only
+  *
+  * The two digits numbers are seperated by a comma and space
+  *
+*/
+
 int main(void)
 {
-		int digit1, digit2;
+	int digit1;
+	int digit2;
 
-			for (digit1 = 0; digit1 < 9; digit1++)
-					{
-								for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-											{
-															putchar((digit1 % 10) + '0');
-																		putchar((digit2 % 10) + '0');
+	for (digit1 = 1; digit1 <= 9; digit1++)
 
-																					if (digit1 == 8 && digit2 == 9)
-																										continue;
-																								putchar(',');
-																											putchar(' ');
-																													}
-									}
+		for (digit2 = digit1 + 1; digit < 10; digit2++)
 
-				putchar('\n');
+			putchar((digit1 % 10) + '0');
 
-					return (0);
+					putchar((digit2 % 10) + '0');
+
+						if(digit1 == 8 && digit2 == 9)
+								continue;
+
+							putchar(',');
+								putchar(' ');
+
+
+
+	putchar('\n');
+
+	return (0);
 }
