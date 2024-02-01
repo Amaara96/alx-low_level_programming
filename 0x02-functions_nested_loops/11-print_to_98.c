ac92969cc855c165b1a000098acfd3c9ecdcc1d6
Amaara <amara.alex96@gmail.com>
2024-02-01 15:31:34 +0000

    Times table

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+  * print_to_98 - A function that prints all natural numbers from n to 98
+  * Return: returns to 0
+  */
+
+void print_to_98(int n)
+{
+
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,11 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-  * main - Prints 9 times table
+  * times_table - Prints 9 times table
   * Return - returns 0
   */
 
 void times_table(void)
 {
+	int a, b, c;
 
-	
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			{
+				printf("%2d", c);
+				if (b != 9)
+					printf(", ");
+			}
+		}
+	printf("\n");
+	}
+}
