467433475c2998e003d42b856b2fd237953f4c11
Amaara <amara.alex96@gmail.com>
2024-02-01 00:16:31 +0000

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * main - Adds two integers
+  * Description - The integers are a and b
+  */
+
+int add(int, int);
+{
+	return(int + int);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+  * main - Prints 9 times table
+  * Return - returns 0
+  */
+
+void times_table(void)
+{
+
+	
