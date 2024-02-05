2c4bd0ce29e992eab2c02ac80d94b0d54c7539ae
Amaara <amara.alex96@gmail.com>
2024-02-05 23:25:33 +0000

    Beyond char

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  * more_numbers - A function that prints from 0 to 14
+  * Return: Always returns to 0
+  */
+
+void more_numbers(void)
+{
+
+	int a, b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			putchar(b / 10 + '0');
+			putchar(b % 10 + '0');
+		}
+
+	putchar('\n');
+
+	}
+}
