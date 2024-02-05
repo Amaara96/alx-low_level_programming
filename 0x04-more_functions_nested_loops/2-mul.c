448193d85fb06ecef050e04c024a5cc9ab32fd63
Amaara <amara.alex96@gmail.com>
2024-02-05 21:15:07 +0000

    Multipication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * mul - A function that multiplies two variables
+  * @a: The first variable
+  * @b: The second variable
+  * Return: The return function multiplies the two variables
+  */
+
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+
+	return (c);
+ 
+}
