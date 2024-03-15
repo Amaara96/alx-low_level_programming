3382aed44e2935ab7f831748370d3b5270e04420
Amaara <amara.alex96@gmail.com>
2024-03-15 14:36:32 +0000

    My new name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints its name and a new character
+ *
+ * Return: The program returns to 0
+ *
+*/
+
+int main(int argc, char *argv[])
+{
+
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+	return(0);
+} 
