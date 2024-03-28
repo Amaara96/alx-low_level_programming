bd7d26e8c2f4dd6f14c3a03ed11ef84a38d7d38a
Amaara <amara.alex96@gmail.com>
2024-03-28 10:30:45 +0000

    mallocs

--- 0x0B-malloc_free/100-main.c
+++ 0x0B-malloc_free/100-main.c
@@ -2,21 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *argstostr(int, char **);
+
 /**
- * main - check the code for ALX School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
-int main(int ac, char *av[])
+int main(void)
 {
-    char *s;
+	char *s;
 
-    s = argstostr(ac, av);
-    if (s == NULL)
-    {
-        return (1);
-    }
-    printf("%s", s);
-    free(s);
-    return (0);
+	s = argstostr(0, NULL);
+	if (s == NULL)
+	{
+		printf("Failed\n");
+		return (1);
+	}
+	printf("%s", s);
+	free(s);
+	return (0);
 }
