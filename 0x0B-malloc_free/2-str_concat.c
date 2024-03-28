e3fa10f5f452a1b26033a9c759831fb07a411c86
Amaara <amara.alex96@gmail.com>
2024-03-28 08:16:01 +0000

    Cocatenation of two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * str_concat - A function that concatenates two strings
+ *
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: returns output
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	int lenght1 = 0;
+	int lenght2 = 0;
+	int a;
+	char *output;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (a = 0; s1[a] != '\0'; a++)
+		lenght1++;
+
+	for (a = 0; s2[a] != '\0'; a++)
+		lenght2++;
+
+	output = malloc(sizeof(char) == (lenght1 + lenght2) + 1);
+
+	if (output == NULL)
+		return (NULL);
+	for (a = 0; s1[a] != '\0'; a++)
+		output[a] = s1[a];
+	for (a = 0; s2[a] != '\0'; a++)
+		output[lenght1 + a] = s2[a];
+
+	return (output);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -7,4 +7,5 @@ A function that creates an array of chars and initializes it with a specific bar
 ## 1-strdup.c
 a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.
 
-
+## 2-str_concat.c
+a function that concatenates two strings.
