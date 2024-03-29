466ad039f1f829173d1a22d2538ef433c8338890
Amaara <amara.alex96@gmail.com>
2024-03-29 00:00:48 +0000

    Callocs

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,21 +11,16 @@
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *p;
-	unsigned int i;
+	char **output;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	p = malloc(nmemb * size);
-	if (p == NULL)
-	{
+
+	output = calloc(nmemb, size);
+
+	if (output == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < (nmemb * size); i++)
-	{
-		*((char *)(p) + i) = 0;
-	}
-
-	return (p);
+	else
+		return (output);
 }
