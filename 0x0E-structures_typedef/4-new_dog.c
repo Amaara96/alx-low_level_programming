a3d581b645b1900c7e565acf806680f63054fad0
Amaara <amara.alex96@gmail.com>
2024-04-01 17:28:29 +0000

    new structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,91 +1,104 @@
 #include <stdlib.h>
 #include "dog.h"
 
-int _strlen(char *s);
-char *_strcpy(char *dest, char *src);
-
 /**
- * new_dog - ...
- * @name: ...
- * @age: ...
- * @owner: ...
+ * _strlen - finds the lenght of a string
  *
- * Return: ...
- */
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *cutie_dog;
-	int name_l = 0, own_l = 0;
-
-	if (name != NULL && owner != NULL)
-	{
-		name_l = _strlen(name) + 1;
-		own_l = _strlen(owner) + 1;
-		cutie_dog = malloc(sizeof(dog_t));
-
-		if (cutie_dog == NULL)
-			return (NULL);
-
-		cutie_dog->name = malloc(sizeof(char) * name_l);
-
-		if (cutie_dog->name == NULL)
-		{
-			free(cutie_dog);
-			return (NULL);
-		}
-
-		cutie_dog->owner = malloc(sizeof(char) * own_l);
-
-		if (cutie_dog->owner == NULL)
-		{
-			free(cutie_dog->name);
-			free(cutie_dog);
-			return (NULL);
-		}
-
-		cutie_dog->name = _strcpy(cutie_dog->name, name);
-		cutie_dog->owner = _strcpy(cutie_dog->owner, owner);
-		cutie_dog->age = age;
-	}
-
-	return (cutie_dog);
-}
-
-/**
- * _strlen - Returns the length of a string
- * @s: String to count
+ * @s: The string
  *
- * Return: String length
+ * Return: Returns the lenght of the string
  */
+
 int _strlen(char *s)
 {
-	int c = 0;
+	int i = 0;
 
-	for (; *s != '\0'; s++)
-	{
-		c++;
-	}
-
-	return (c);
+	while (s[i] != '\0')
+		i++;
+	return (i);
 }
 
 /**
- * _strcpy - Copy a string
- * @dest: Destination value
- * @src: Source value
+ * _strcpy - To copy the string s
  *
- * Return: the pointer to dest
+ * @dest: destination of the string
+ *
+ * @src: source of the string
+ *
+ * Return: returns the copied string
  */
+
 char *_strcpy(char *dest, char *src)
 {
+	int len;
 	int i;
 
-	for (i = 0; src[i] != '\0'; i++)
-	{
+	len = _strlen(src);
+	for (i = 0; i < len; i++)
 		dest[i] = src[i];
-	}
-
-	dest[i++] = '\0';
-
+	dest[i] = '\0';
 	return (dest);
 }
+
+/**
+ * new_dog - creates new dog
+ *
+ * @name: name of the dog
+ *
+ * @age: age of the dog
+ *
+ * @owner: Owner of the dog
+ *
+ * Return: returns to 0
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ndog;
+	int len1, len2;
+
+	len1 = _strlen(name);
+	len2 = _strlen(owner);
+
+	ndog = malloc(sizeof(dog_t));
+	if (ndog == NULL)
+		return (NULL);
+
+	ndog->name = malloc(sizeof(char) * (len1 + 1));
+	if (ndog->name == NULL)
+	{
+		free(ndog);
+		return (NULL);
+	}
+	ndog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (ndog->owner == NULL)
+	{
+		free(ndog->name);
+		free(ndog);
+		return (NULL);
+	}
+	_strcpy(ndog->name, name);
+	_strcpy(ndog->owner, owner);
+	ndog->age = age;
+	return (ndog);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
