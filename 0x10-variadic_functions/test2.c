dacdd30f1db6904070cd459ac05022edc1e6176e
Amaara <amara.alex96@gmail.com>
2024-04-16 10:57:34 +0000

    Argument count

---
+++ 0x10-variadic_functions/test2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum(int ar, ...);
+
+int main(void)
+{
+	sum(4, 1, 2, 3,5);
+}
+int sum(int ar, ...)
+{
+	int i; /* a variable for the looping */
+	int sum  = 0;
+	
+	va_list arg; /* declares the variable arg*/
+	va_start(arg, ar); /* initializes arg*/
+	for (i = 0; i < ar; i++)
+	{
+		sum = sum + va_arg(arg, int);  /* the looping takes place here by fetching the elements one by one */
+	}
+	printf("%d\n", sum);
+	va_end(arg); /*ends the operation*/
+}
