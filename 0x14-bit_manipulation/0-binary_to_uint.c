83157665a5f50b1e4dae902955f74811f1d847b0
Amaara <amara.alex96@gmail.com>
2024-05-12 23:11:36 +0000

    smart

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_unit - This is a function that converts binary number
+ * To unsigned int
+ * 
+ * @b: This is the pointer to the string
+ *
+ * @NULL: This is the return value for nothing
+ *
+ * @Return: It returns to the value
+*/
+
+unsigned int binary_to_unit(const char *b)
+
+	char b = 0;
+	unsigned int i = 0;
+
+	if b = NULL
+	{
+		return (i);
+	
+	}
+	else if (char b = char++)
+	{
+		return (i);
+	}
+	else
+	{
+		return (0);
+	}
+
---
+++ 0x14-bit_manipulation/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	unsigned int n;
+
+	n = binary_to_unit("1");
+	printf("%u\n", n);
+	n = binary 
